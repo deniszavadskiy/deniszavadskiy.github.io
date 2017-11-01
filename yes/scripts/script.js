a5dbe2c3e114b5351e4eeb64b15922df79e2f497
Denis Zavadskiy <denis.v.zavadskiy@gmail.com>
2017-11-02 01:13:56 +0200

    update(*): made fixes

--- yes/scripts/script.js
+++ yes/scripts/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 			event.preventDefault();
 			var top = $(id).offset().top,
 				height = $("#sectionHeader").height();
-			$('body,html').animate({scrollTop: top - height}, 1000);
+			$('body,html').animate({scrollTop: top + height - 70}, 1000);
 		}
 	});
 
@@ -36,3 +36,27 @@ $(document).ready(function(){
 		$('#mobileMenuWrapper').toggleClass('active');
 	}
 });
+
+
+$(document).on("scroll", onScroll);
+
+
+function onScroll(){
+	var menu_selector = "#menu";
+	var scroll_top = $(document).scrollTop();
+	$(menu_selector + " a").each(function(){
+		var hash = $(this).attr("href"),
+			height = $("#sectionHeader").height();;
+		if (hash[0] == "#") {
+			var target = $(hash);
+			console.log(target.outerHeight());
+			if (target.position().top <= scroll_top && target.position().top + target.outerHeight() > scroll_top ) {
+				$(menu_selector + " a.active").removeClass("active");
+				$(this).addClass("active");
+			} else {
+				$(this).removeClass("active");
+			}
+		}
+	});
+
+}
